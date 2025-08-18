dbb4597169f1abe118c503c1edeb87cbc527b65e
utumbo <121032782+utumbo@users.noreply.github.com>
2025-08-19 01:05:26 +0700

    create Hello world
    Create Hello, FastApi

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def hello_index():
-    return {
-        "message": "Hello, World!"
-    }
-
-@app.get("/index/")
-def list_items():
-    return {
-        "item1",
-        "item2",
-        "item3",
-        "fjnsf"
-    }
+@app.get("/")
+async def root():
+    return {"Hello, FastApi"}
