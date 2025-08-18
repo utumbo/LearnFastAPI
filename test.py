907c20277ae9514ebeb80244c59dabeda35f9248
utumbo <121032782+utumbo@users.noreply.github.com>
2025-08-19 01:30:40 +0700

    srong task
    create strong task return index.html

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {
-        'message': 'Авторелоад правда работает'
-    }
+    return FileResponse("index.html")
