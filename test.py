01a702a51c653014263b1c94d04d3dcd5ee439c5
utumbo <121032782+utumbo@users.noreply.github.com>
2025-08-19 01:37:04 +0700

    create calc
    create def calculate

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return FileResponse("index.html")
+
+@app.post("/calculate")
+async def calculate(num1: float, num2: float):
+    return {"result": num1 + num2}
