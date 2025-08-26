212b930ce6e22d5e7e24865f822d4fe505f8703a
utumbo <121032782+utumbo@users.noreply.github.com>
2025-08-26 23:14:59 +0700

    create new routes
    create new routes

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World!"}
+
+# новый маршрут
+@app.get("/custom")
+async def read_custom_message():
+    return {"message": "This is a custom message!"}
--- test.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.post("/calculate")
-async def calculate(num1: float, num2: float):
-    return {"result": num1 + num2}
